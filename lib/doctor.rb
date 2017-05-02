ba2175e8df1bc238f603aa41a526a8397b80918d
Aaron Nguyen and Max Reichman <max@>
2017-05-02 15:45:44 -0700

    Add methods to find every doctor for each specialty, and return a list of all doctors in alphabetical order with the amount of patients for each.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -27,4 +27,42 @@ class Doctor
   define_method(:==) do |another_doctor|
     self.name.==(another_doctor.name).&(self.id.==(another_doctor.id))
   end
+
+  define_singleton_method(:doctors_in_specialty) do |specialty_name|
+    specialty = specialty_name
+    returned_specialty = DB.exec("SELECT * FROM specialties WHERE name = '#{specialty}';")
+    specialty_id = nil
+    returned_specialty.each() do |specialty|
+      specialty_id = specialty.fetch("id").to_i()
+    end
+    doctors_list = DB.exec("SELECT * FROM doctors WHERE specialty_id = #{specialty_id};")
+    doctors_array = []
+    doctors_list.each() do |doctor|
+      name = doctor.fetch("name")
+      specialty_id = doctor.fetch("specialty_id").to_i()
+      id = doctor.fetch("id").to_i()
+      doctors_array.push(Doctor.new({:name => name, :specialty_id => specialty_id, :id => id}))
+    end
+    doctors_array
+  end
+
+  define_singleton_method(:patients_for_doctor) do
+    returned_doctors = DB.exec("SELECT * FROM doctors ORDER BY name;")
+    doctors = []
+    returned_doctors.each() do |doctor|
+      name = doctor.fetch("name")
+      specialty_id = doctor.fetch("specialty_id").to_i()
+      id = doctor.fetch("id").to_i()
+      doctors.push(Doctor.new({:name => name, :specialty_id => specialty_id, :id => id}))
+    end
+    doctor_patient_array = []
+    returned_doctors.each() do |doctor|
+      id = doctor.fetch("id").to_i()
+      patients_amount = DB.exec("SELECT COUNT(name) FROM patients WHERE doctor_id = #{id};")
+      amount = patients_amount.values().join().to_s()
+      name = doctor.fetch("name")
+      doctor_patient_array.push(name + amount)
+    end
+    doctor_patient_array
+  end
 end
--- spec/doctor_spec.rb
+++ spec/doctor_spec.rb
@@ -21,4 +21,30 @@ describe(Doctor) do
       expect(Doctor.all()).to(eq([test_doctor]))
     end
   end
+
+  describe(".doctors_in_specialty") do
+    it("finds all doctors that are in the specialty") do
+      test_specialty = Specialty.new({:name => "Radiologist", :id => nil})
+      test_specialty.save()
+      test_doctor = Doctor.new({:name => "Aaron", :specialty_id => test_specialty.id, :id => nil})
+      test_doctor.save()
+      expect(Doctor.doctors_in_specialty("Radiologist")).to(eq([test_doctor]))
+    end
+  end
+
+  describe(".patients_for_doctor") do
+    it("Shows each doctor in alphabetical order with number of patients they see") do
+      test_doctor1 = Doctor.new({:name => "Max", :specialty_id => 2, :id => nil})
+      test_doctor1.save()
+      test_doctor2 = Doctor.new({:name => "Aaron", :specialty_id => 1, :id => nil})
+      test_doctor2.save()
+      test_patient1 = Patient.new({:name => "patient1", :doctor_id => test_doctor1.id, :id => nil})
+      test_patient1.save()
+      test_patient2 = Patient.new({:name => "patient2", :doctor_id => test_doctor2.id, :id => nil})
+      test_patient2.save()
+      test_patient3 = Patient.new({:name => "patient3", :doctor_id => test_doctor1.id, :id => nil})
+      test_patient3.save()
+      expect(Doctor.patients_for_doctor()).to(eq(["Aaron1", "Max2"]))
+    end
+  end
 end
